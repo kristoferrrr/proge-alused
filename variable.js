d296f1acf36001f3d7639bf89c0f74c1acd565bd
KristoferJakobson <kristofer.jakobson@voco.ee>
2023-09-19 12:02:48 +0300

    js variable - declare, assign and use

---
+++ variable.js
@@ -0,0 +1,19 @@
+// variables
+// keyword variable name = value;
+// let
+
+// declare
+let programminglanguage = 'Java script'
+console.log(programminglanguage)
+
+// assigment - save new value into the variable
+programminglanguage = 'PHP'
+console.log(programminglanguage)
+
+let result
+console.log(result)
+
+// const
+const group = 'VS23'
+group = 'PG23'
+console.log(group)
