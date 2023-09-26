1ad9e9bb3a526fe959d994fbdd2361453355a1cf
KristoferJakobson <kristofer.jakobson@voco.ee>
2023-09-26 12:07:47 +0300

    basic math operators and math package

---
+++ math.js
@@ -0,0 +1,23 @@
+let number1 = 100
+let number2 = 50
+
+let result
+
+result = number1 + number2
+result = number1 - number2
+result = number1 * number2
+result = number1 / number2
+
+result = Math.PI
+result = Math.round(100.8)
+result = Math.round(100.2)
+
+let value1 = Math.floor(100.6) // - down
+let value2 = Math.ceil(100.3) // - up
+result = Math.sqrt(64)
+result = Math.pow(8, 2)
+result = Math.abs(-3)
+
+result = Math.round(Math.random()*10)
+
+console.log(result)
